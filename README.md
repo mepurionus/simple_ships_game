57c6b05eae9ea9d1ee68c198f7a9895ec3a435fb
Mepuri <mepurionus@gmail.com>
2021-09-17 10:05:59 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Simple Ships Game
+
+## I made that game out of boredom waiting for my laptop to turn on.
+
+### Version 0.0.1
---
+++ main.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    do {
+        system("cls");
+        string visibletab[10][10];
+        int hiddentab[10][10];
+        int l[10];
+        int hitsleft = 10;
+        int hitships = 0;
+        int triesintotal = 1;
+
+        for(int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y ++) {
+                visibletab[x][y] = "O";
+                hiddentab[x][y] = 0;
+            }
+        }
+        for (int k = 0; k < 10; k++) {
+         int y = rand()%10;
+         if(hiddentab[k][y] != 1) hiddentab[k][y] = 1;
+         else k--;
+        }
+
+        do {
+        system("cls");
+        cout << "Legend: \nQ - missed\nX - hit\nO - not checked" << endl;
+        cout << hitsleft << " ships left!\nU hit " << hitships << " ships" << endl;
+        cout << triesintotal << " tries in total!" << endl;
+
+        int zmx, zmy;
+        for(int x = 0; x < 10; x++) {
+            for (int y = 0; y < 10; y ++) {
+                cout << visibletab[x][y] << "|";
+            }
+            cout << endl;
+        }
+        cout << endl;
+
+        cout << "Podaj X oraz Y: ";
+        cin >> zmy >> zmx;
+        if(hiddentab[zmx-1][zmy-1] == 1) {
+            visibletab[zmx-1][zmy-1] = "X";
+            hitsleft = hitsleft - 1;
+            hitships = hitships + 1;
+        } else {
+            visibletab[zmx-1][zmy-1] = "N";
+        }
+        triesintotal = triesintotal + 1;
+        } while (hitsleft > 0);
+        cout << "Congratulations! U hit all ships possible in the game with " << triesintotal << "ties!";
+        system("pause >nul");
+        break;
+    } while (true);
+
+}
